b62f45e60c64e568cd248cb9f2ba4da657653768
aceva <aceva@alumni.uv.es>
2026-04-28 00:25:01 +0200

    ptriang function completed. Tests done succesfully.

--- R/triang_dist.R
+++ R/triang_dist.R
@@ -7,22 +7,58 @@ dtriang <- function(x, min, max, mode) {
     stop("min can't be greater than max")
   }
 
-  if (mode < min | mode > max) {
+  if (mode < min || mode > max) {
     stop("mode must be between min and max.")
   }
 
-  total_area <- (mode - min)/(max-min) + (max - mode)/(max-min)
-
-  if (x < min | x > max) {
-    resultado <- 0
-    return(resultado)
+  if (x < min || x > max) {
+    result <- 0
+    return(result)
   }
 
   if (x <= mode) {
-    resultado <-  2 * (x - min) / ((max - min) * (mode - min))
+    result <-  2 * (x - min) / ((max - min) * (mode - min))
   } else {
-    resultado <- 2 * (max - x) / ((max - min) * (max - mode))
+    result <- 2 * (max - x) / ((max - min) * (max - mode))
   }
 
-  return(resultado)
+  result
+}
+
+#' Triangular density
+#'
+#' @param x numeric vector
+#' @param min left limit
+#' @param max right limit
+#' @param mode mode
+#' @return PDF result
+#' @export
+
+ptriang <- function(q, min, max, mode) {
+  if (class(q) != "numeric") {
+    stop("x must be a numeric vector or a number.")
+  }
+  if (min > max) {
+    stop("min can't be greater than max")
+  }
+
+  if (mode < min || mode > max) {
+    stop("mode must be between min and max.")
+  }
+
+  if (q <= min) {
+    result <- 0
+    return(result)
+  }
+  if (q >= max) {
+    result <- 1
+    return(result)
+  }
+  if (q <= mode) {
+    result <- (q - min)^2 / ((max - min) * (mode - min))
+  }
+  if (q >= mode) {
+    result <- 1 - (max - q)^2 / ((max - min) * (max - mode))
+  }
+  result
 }
--- tests/testthat/test-triang_dist.R
+++ tests/testthat/test-triang_dist.R
@@ -5,4 +5,16 @@ test_that("dtriang tests", {
   expect_error(dtriang(0.5, min = 0, max = 1, mode = 2))
   expect_equal(dtriang(0.5, 0, 1, 0.5), 2)
   expect_error(dtriang(0.5, min = 0, max = 1, mode = -1))
+  expect_true(dtriang(0.8, 0, 1, 0.5) > 0)
+})
+
+
+test_that("ptriang tests", {
+  expect_equal(ptriang(0, 0, 1, 0.5), 0)
+  expect_equal(ptriang(1, 0, 1, 0.5), 1)
+  expect_equal(ptriang(0.5, 0, 1, 0.5), 0.5)
+  expect_error(ptriang(0.5, 1, 0, 0.5))
+  expect_error(ptriang(0.5, min = 0, max = 1, mode = 2))
+  expect_equal(ptriang(2, 0, 1, 0.5), 1)
+  expect_equal(ptriang(1, 0, 1, 0.5), 1)
 })
